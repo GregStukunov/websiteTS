a7d8263dbeeb132a9fa8b099ee805062373c60c3
Grigory Stukunov <gstukunov@devexperts.com>
2022-06-24 17:45:48 +0300

    architecture

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MainPage } from "./pages/main-page/main-page";
+import { Vacancies } from "./pages/vacancies/vacancies";
+import { AboutUs } from "./pages/about-us/about-us";
+import { Contacts } from "./pages/contacts/contacts";
+import { OurCrew } from "./pages/our-crew/our-crew";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="vacancies" element={<Vacancies />} />
+      <Route path="about-us" element={<AboutUs />} />
+      <Route path="our-crew" element={<OurCrew />} />
+      <Route path="contacts" element={<Contacts />} />
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
